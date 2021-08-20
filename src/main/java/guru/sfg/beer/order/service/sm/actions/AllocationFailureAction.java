bfe5d53da58cb016a7cf563d05a991449af1577c
schwendh <schwendh>
2021-08-20 14:29:25 +0200

    247. Cancel Order Configuration

--- src/main/java/guru/sfg/beer/order/service/sm/actions/AllocationFailureAction.java
+++ src/main/java/guru/sfg/beer/order/service/sm/actions/AllocationFailureAction.java
@@ -29,8 +29,8 @@ public class AllocationFailureAction implements Action<BeerOrderStatusEnum, Beer
         String beerOrderId = (String) context.getMessage().getHeaders().get(BeerOrderManagerImpl.ORDER_ID_HEADER);
 
         jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_FAILURE_QUEUE, AllocationFailureEvent.builder()
-                .orderId(UUID.fromString(beerOrderId))
-                .build());
+            .orderId(UUID.fromString(beerOrderId))
+                    .build());
 
         log.debug("Sent Allocation Failure Message to queue for order id " + beerOrderId);
     }
